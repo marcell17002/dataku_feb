2e152454fa6bfceacd8de473acedfd1517ac5c03
victorwijaya34 <victorwijaya62@gmail.com>
2020-05-14 14:30:57 +0700

    upadete db

--- src/db_simpeg.sql
+++ src/db_simpeg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2020 at 06:21 PM
+-- Generation Time: May 14, 2020 at 09:30 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -31,29 +31,57 @@ SET time_zone = "+00:00";
 CREATE TABLE `data_karyawan` (
   `id` int(12) NOT NULL,
   `nama` varchar(50) NOT NULL,
+  `ktp` varchar(16) NOT NULL,
   `JK` varchar(10) NOT NULL,
   `tmp_lhr` varchar(25) NOT NULL,
   `tgl_lhr` date NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `divisi` varchar(50) NOT NULL,
   `jabatan` varchar(25) NOT NULL,
   `no_telp` varchar(13) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `suamiistri` varchar(10) NOT NULL,
-  `anak` int(10) NOT NULL,
-  `pajak` int(12) NOT NULL,
-  `bpjs` int(12) NOT NULL,
-  `denda` int(12) NOT NULL
+  `anak` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `data_karyawan`
 --
 
-INSERT INTO `data_karyawan` (`id`, `nama`, `JK`, `tmp_lhr`, `tgl_lhr`, `jabatan`, `no_telp`, `alamat`, `suamiistri`, `anak`, `pajak`, `bpjs`, `denda`) VALUES
-(28, 'Harjo', 'Pria', 'sukawening', '2020-04-03', 'Marketing', '012351231', 'Sayang 23', 'Ya', 1, 100000, 20000, 30000),
-(29, 'Budi', 'Pria', 'rosario', '2020-04-02', 'Manager', '123123', 'Test Halaman', 'Ya', 4, 1000, 1000, 1000),
-(30, 'Dapil', 'Pria', 'solo', '2020-04-04', 'Manager', '123', 'rumah dapil', 'Ya', 1, 1000000, 100000, 200000),
-(31, 'sumi', 'Wanita', 'sapporo', '2020-04-02', 'Keuangan', '564543', 'tokyo', 'Tidak', 0, 200000, 300000, 100000),
-(32, 'boruto', 'Pria', 'konoha', '2020-04-09', 'Tech', '09876', 'konoha', 'Tidak', 0, 150000, 230000, 100000);
+INSERT INTO `data_karyawan` (`id`, `nama`, `ktp`, `JK`, `tmp_lhr`, `tgl_lhr`, `tgl_masuk`, `divisi`, `jabatan`, `no_telp`, `alamat`, `suamiistri`, `anak`) VALUES
+(34, 'Heri', '1234567890123456', 'Pria', 'Jakarta', '2020-04-07', '2020-05-06', 'Kredit', 'Manager', '123412313', 'rumah dapil ', 'Ya', 1),
+(36, 'Onad', '1233451235323', 'Pria', 'Depok', '2020-05-28', '0000-00-00', 'Kredit', 'Board of Manager', '5476546', 'Bintaro', 'Ya', 1),
+(37, 'Budi', '1234567890123455', 'Pria', 'Jakarta', '2020-05-18', '0000-00-00', 'Kredit', 'Manager', '08922222222', 'Menteng', 'Ya', 1),
+(38, 'Gado', '1343521334', 'Pria', 'Tokyo', '2020-05-06', '0000-00-00', 'IT', 'Board of Manager', '12312323', 'ginza 12', 'Tidak', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `total` int(14) NOT NULL,
+  `file_hrd` varchar(255) NOT NULL,
+  `tgl_serah` date NOT NULL,
+  `departemen` varchar(25) NOT NULL,
+  `file_keuangan` varchar(200) NOT NULL,
+  `pre_number` varchar(12) NOT NULL,
+  `tgl_pengajuan` date NOT NULL,
+  `bukti_bayar` varchar(200) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `deskripsi`, `total`, `file_hrd`, `tgl_serah`, `departemen`, `file_keuangan`, `pre_number`, `tgl_pengajuan`, `bukti_bayar`, `status`) VALUES
+(1, 'Gaji Karyawan', 100000000, 'Proposal Earthday.pdf', '2020-05-08', 'HRD', 'Proposal Earthday.pdf', '001', '2020-05-08', 'VictorWijaya_CV.pdf', 'Pending'),
+(3, 'Gaji Staff', 200000000, 'Proposal Earthday.pdf', '2020-05-08', 'HRD', 'Proposal Earthday.pdf', '001', '2020-05-08', '', 'Pending'),
+(5, 'Gaji Staff', 50000000, '', '2020-05-08', '', '', '', '0000-00-00', '', '');
 
 -- --------------------------------------------------------
 
@@ -86,6 +114,12 @@ ALTER TABLE `data_karyawan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,7 +127,13 @@ ALTER TABLE `data_karyawan`
 -- AUTO_INCREMENT for table `data_karyawan`
 --
 ALTER TABLE `data_karyawan`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

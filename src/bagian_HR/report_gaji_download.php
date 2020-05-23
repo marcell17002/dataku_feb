387c563885c042f7b45ad37aa2be061e3873c3c5
victorwijaya34 <victorwijaya62@gmail.com>
2020-05-23 15:37:26 +0700

    mpdf

--- src/bagian_HR/report_gaji_download.php
+++ src/bagian_HR/report_gaji_download.php
@@ -3,14 +3,18 @@
     session_start();
     require '../config.php';
 
-    $karyawan = query("SELECT * FROM data_karyawan");
-
-    $id = '1';
+    $id = $_GET["id"];
+    $karyawan = query("SELECT * FROM data_karyawan WHERE id = $id");
 
     $mpdf = new \Mpdf\Mpdf();
 
-    $data = '';
-    $data .= ' 
+    $html = '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Daftar Mashasiswa</title>
+    </head>
+    <body>
         <table style=" border-collapse: collapse;width:100%;">
         <tr>
             <th style="text-align:center" >KONSULTASI PAJAK EKOS PARTNER</th> 
@@ -23,31 +27,44 @@
         </tr>
         </table><br>
         <table class="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">'.$id.'</th>
-            <th scope="col">Divisi</th>
-            <th scope="col">Jabatan</th>
-            <th scope="col">Gaji Pokok</th>
-            <th scope="col">Pajak Bulanan</th>
-            <th scope="col">Gaji Akhir</th>
-          </tr>
-        </thead>
-        </table>';
-    // foreach( $karyawan as $row) :
-    // $data .= '
-    // <tr>
-    //   <td>'. $row["nama"].'</td>
-    //   <td><'.$row["divisi"].'></td>
-    //   <td><'.$row["jabatan"].'></td>
-    //   <td><'.$row["gajiperbulan"].'></td>
-    //   <td><'.$row["pajakbulanan"].'></td>
-    //   <td>'.$row["gajiakhir"].'</td>
-    // </tr> 
-    // </tbody>
-    // </table>';
-    // endforeach;
-    $mpdf ->WriteHTML($data);
+        <thead>';
+
+        foreach($karyawan as $row){
+            $html .= '
+                <tr>
+                    <td>Nama            : </td>
+                    <td>'. $row["nama"] .'</td>
+                </tr>
+                <tr>
+                    <td>Divisi          : </td>
+                    <td>'. $row["divisi"] .'</td>
+                </tr>
+                <tr>
+                    <td>Jabatan         : </td>
+                    <td>'. $row["jabatan"] .'</td>
+                </tr>
+                <tr>
+                    <td>Gaji Pokok      : </td>
+                    <td>'. $row["gajiperbulan"] .'</td>
+                </tr>
+                <tr>
+                    <td>Pajak Bulanan   : </td>
+                    <td>'. $row["pajakbulanan"] .'</td>
+                </tr>
+                <tr>
+                    <td>Gaji Akhir      : </td>
+                    <td>'. $row["gajiakhir"] .'</td>
+                </tr>
+                ';
+        }        
+
+    $html .= '
+    </thead>
+    </table>
+    </body>
+    </html>';
+    
+    $mpdf ->WriteHTML($html);
     $mpdf->Output('slip-gaji-karyawan.pdf','D');
     header('Location: report_gaji.php?status=sukses');
 
